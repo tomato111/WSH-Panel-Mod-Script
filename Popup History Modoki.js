77f4a0db7a4490aaab46c436a9aa61a2d33ddc93
tomato111 <ashiatoxx@gmail.com>
2016-04-19 16:30:36 +0900

    Popup History Modoki 1.1.0

--- Popup History Modoki.js
+++ Popup History Modoki.js
@@ -1,6 +1,6 @@
 ﻿// ==PREPROCESSOR==
 // @name "Popup History Modoki"
-// @version "1.0.1"
+// @version "1.1.0"
 // @author "tomato111"
 // ==/PREPROCESSOR==
 
@@ -12,7 +12,7 @@ var prop = new function () {
 
     this.Menu = {
         ItemName: window.GetProperty('Menu.ItemName', '%title%'),
-        MaxSize: window.GetProperty('Menu.MaxSize', 10)
+        MaxSize: window.GetProperty('Menu.MaxSize', 25)
     };
 
     this.Style = {
@@ -27,7 +27,7 @@ var prop = new function () {
 
 
 //============================================
-//== function ==================================
+//== function ================================
 //============================================
 
 function RGBA(r, g, b, a) {
@@ -38,16 +38,26 @@ function RGBA(r, g, b, a) {
 
 
 //============================================
-//== Popup History Modoki Object ==========================
+//== Popup History Modoki Object =============
 //============================================
 var PHM = new function () {
+    var history_items, history_index_data, restoreHistory, buildHistoryMenu, _menu;
+
+    var MF_STRING = 0x00000000;
+    var MF_GRAYED = 0x00000001;
+    var MF_CHECKED = 0x00000008;
+    var MF_SEPARATOR = 0x00000800;
+
+
     this.on_paint = function (gr) {
         gr.FillSolidRect(-1, -1, window.Width + 2, window.Height + 2, prop.Style.Color.Background);
     };
 
     this.init = function () {
         history_items = [];
-        buildHistoryMenu();
+        history_index_data = [];
+        restoreHistory();
+        buildHistoryMenu(fb.IsPlaying && fb.GetNowPlaying());
     };
 
     this.add = function (metadb) {
@@ -59,39 +69,102 @@ var PHM = new function () {
             }
         }
 
-        history_items.unshift(new Info(metadb));
-        if (history_items.length > prop.Menu.MaxSize)
-            history_items.pop();
+        history_items.unshift(new Info(metadb, plman.PlayingPlaylist));
+        history_index_data.unshift(
+            [
+                history_items[0].playlistIndex,
+                Math.floor(history_items[0].metadb.Length * 1000)
+            ]
+        );
 
+        history_items.length = Math.min(history_items.length, prop.Menu.MaxSize);
+        history_index_data.length = history_items.length;
+
+        window.SetProperty('SystemData', history_index_data.join('^'));
         buildHistoryMenu(metadb);
+    };
+
+    this.clear = function (x, y) {
+        var ret =
+            buildMenu([
+                { Flag: MF_GRAYED, Caption: 'Clear list?' },
+                { Flag: MF_SEPARATOR },
+                { Flag: MF_STRING, Caption: 'OK' },
+                { Flag: MF_STRING, Caption: 'Cancel' }
+            ]).TrackPopupMenu(x, y);
+        if (ret === 3) {
+            history_items = [];
+            history_index_data = [];
+            window.SetProperty('SystemData', '');
+            buildHistoryMenu();
+        }
     };
 
     this.popup = function (x, y) {
         var ret = _menu.TrackPopupMenu(x, y);
-        if (ret !== 0)
+        if (ret === history_items.length + 2)
+            this.clear(x, y);
+        else if (ret !== 0)
             history_items[ret - 1].doCommand();
     };
 
 
-    var history_items, _menu;
+    restoreHistory = function () {
+        var str = window.GetProperty('SystemData');
+        if (!str) {
+            return;
+        }
 
-    var MF_STRING = 0x00000000;
-    var MF_GRAYED = 0x00000001;
-    var MF_CHECKED = 0x00000008;
+        var playlistIndex, playbackLength, HandleList, metadb;
 
+        history_index_data = str.split('^');
+        for (var i = 0; i < history_index_data.length;) {
+            history_index_data[i] = eval('[' + history_index_data[i] + ']');
 
-    var buildHistoryMenu = function (metadb) {
+            playlistIndex = history_index_data[i][0];
+            playbackLength = history_index_data[i][1];
+
+            HandleList = plman.GetPlaylistItems(playlistIndex);
+            for (var j = 0; j < HandleList.Count; j++) {
+                if (Math.floor(HandleList.Item(j).Length * 1000) === playbackLength)
+                    break;
+            }
+
+            if (j !== HandleList.Count) {
+                history_items.push(new Info(HandleList.Item(j), playlistIndex));
+                i++;
+            }
+            else
+                history_index_data.splice(i, 1);
+        }
+
+        history_items.length = Math.min(history_items.length, prop.Menu.MaxSize);
+        history_index_data.length = history_items.length;
+
+        window.SetProperty('SystemData', history_index_data.join('^'));
+    };
+
+    buildHistoryMenu = function (metadb) {
 
         var menu_items = [];
         if (history_items.length) {
             for (var i = 0; i < history_items.length; i++) {
                 menu_items.push(
                     {
-                        Flag: metadb.Compare(history_items[i].metadb) ? MF_CHECKED : MF_STRING,
+                        Flag: metadb ? (metadb.Compare(history_items[i].metadb) ? MF_CHECKED : MF_STRING) : MF_STRING,
                         Caption: history_items[i].name
                     }
                 );
             }
+            menu_items.push(
+                {
+                    Flag: MF_SEPARATOR
+                },
+                {
+                    Flag: MF_STRING,
+                    Caption: '(Clear)'
+                }
+            );
         }
         else {
             menu_items.push(
@@ -115,14 +188,14 @@ var PHM = new function () {
     }
 
     // Constructor
-    function Info(metadb) {
+    function Info(metadb, playlistIndex) {
         this.metadb = metadb;
         this.name = fb.TitleFormat(prop.Menu.ItemName).EvalWithMetadb(metadb);
-        this.itemLocation = plman.GetPlayingItemLocation();
+        this.playlistIndex = playlistIndex;
     }
     Info.prototype.doCommand = function () {
         var metadb = this.metadb;
-        var playlistIndex = this.itemLocation.PlaylistIndex;
+        var playlistIndex = this.playlistIndex;
 
         plman.SetPlaylistFocusItemByHandle(playlistIndex, metadb);
 
@@ -143,7 +216,7 @@ var PHM = new function () {
 
 
 //========================================
-//== Callback function =========================
+//== Callback function ===================
 //========================================
 function on_paint(gr) {
     PHM.on_paint(gr);
